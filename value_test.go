a831acf48e24a321afb0682ba1db536b970a733b
Patrick Pokatilo <mail@shyxormz.net>
2017-07-05 17:32:36 +0200

    Decorate value tests with contexts

--- value_test.go
+++ value_test.go
@@ -30,28 +30,32 @@ var _ = Describe("Value", func() {
 		server.Close()
 	})
 
-	It("Can encode slices", func() {
-		verifyAndRespond(
-			`<?xml version="1.0"?><methodCall><methodName>test</methodName><params><param><value><array><data><value><string>foo</string></value><value><string>bar</string></value></data></array></value></param></params></methodCall>`,
-			`<?xml version="1.0"?><methodResponse><params><param><boolean>true</boolean></param></params></methodResponse>`,
-		)
+	Context("Encoding slices", func() {
+		It("Can encode two strings", func() {
+			verifyAndRespond(
+				`<?xml version="1.0"?><methodCall><methodName>test</methodName><params><param><value><array><data><value><string>foo</string></value><value><string>bar</string></value></data></array></value></param></params></methodCall>`,
+				`<?xml version="1.0"?><methodResponse><params><param><boolean>true</boolean></param></params></methodResponse>`,
+			)
 
-		_, err := client.Call("test", []interface{}{"foo", "bar"})
+			_, err := client.Call("test", []interface{}{"foo", "bar"})
 
-		Expect(err).To(BeNil())
+			Expect(err).To(BeNil())
+		})
 	})
 
-	It("Can decode slices", func() {
-		verifyAndRespond(
-			`<?xml version="1.0"?><methodCall><methodName>test</methodName><params></params></methodCall>`,
-			`<?xml version="1.0"?><methodResponse><params><param><value><array><data><value><string>foo</string></value><value><string>bar</string></value></data></array></value></param></params></methodResponse>`,
-		)
+	Context("Decoding slices", func() {
+		It("Can decode two slices", func() {
+			verifyAndRespond(
+				`<?xml version="1.0"?><methodCall><methodName>test</methodName><params></params></methodCall>`,
+				`<?xml version="1.0"?><methodResponse><params><param><value><array><data><value><string>foo</string></value><value><string>bar</string></value></data></array></value></param></params></methodResponse>`,
+			)
 
-		val, err := client.Call("test")
+			val, err := client.Call("test")
 
-		Expect(err).To(BeNil())
-		Expect(len(val.AsArray())).To(Equal(2))
-		Expect(val.AsArray()[0].AsString()).To(Equal("foo"))
-		Expect(val.AsArray()[1].AsString()).To(Equal("bar"))
+			Expect(err).To(BeNil())
+			Expect(len(val.AsArray())).To(Equal(2))
+			Expect(val.AsArray()[0].AsString()).To(Equal("foo"))
+			Expect(val.AsArray()[1].AsString()).To(Equal("bar"))
+		})
 	})
 })
