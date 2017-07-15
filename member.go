bc369d17f4f0e72b91d66df4bc34b9e70318dfde
Patrick Pokatilo <mail@shyxormz.net>
2017-07-15 16:33:42 +0200

    Minor update to documentation for Member

--- member.go
+++ member.go
@@ -2,6 +2,9 @@ package xmlrpc
 
 // Member represents a member in a struct.
 type Member interface {
+	// Name returns the name of the struct's member.
 	Name() string
+
+	// Value returns the value of the struct's member.
 	Value() Value
 }
